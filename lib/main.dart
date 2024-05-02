1d871b070d11ff201f1d34d50b51b8bd667b798d
Guy Ahonakpon GBAGUIDI <guygbaguidi123root@gmail.com>
2024-05-02 15:38:42 +0100

    perf: init the position using the user location and control map from outside

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
         enableTracking: true,
         unFollowUser: false,
       ),
+      useExternalTracking: true,
     );
 
     return Scaffold(
